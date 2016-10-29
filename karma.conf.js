5b794fad3ee9b8e3bee5c6f0f7cb7c0b8edb9e0c
revente <aurora_trd21@hotmail.com>
2016-10-29 14:24:38 +0000

    add countdown

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 var webpackConfig = require('./webpack.config.js');
 module.exports = function(config) {
   config.set({
-    browsers: ['Chrome'],
+    browsers: ['Chromium'],
     singleRun: true,
     frameworks: ['mocha'],
     files: [
