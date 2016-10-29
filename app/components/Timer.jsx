955384deb52698641ca7123e00d4059315216eee
revente <aurora_trd21@hotmail.com>
2016-10-29 21:05:49 +0000

    Created timer component

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,10 +1,57 @@
 var React = require('react');
 
+var Clock = require('Clock');
+var Controls = require('Controls');
+var CountdownForm = require('CountdownForm');
+
 var Timer = React.createClass({
+  getInitialState: function() {
+    return({
+      timerStatus: 'stopped',
+      count: 0
+    });
+  },
+  componentDidUpdate: function(prevProps, prevState) {
+    if (this.state.timerStatus !== prevState.timerStatus) {
+      switch(this.state.timerStatus){
+        case 'started':
+          this.startTimer();
+          break;
+        case 'stopped':
+          this.setState({
+            count: 0,
+          });
+        case 'paused':
+          clearInterval(this.timer);
+          this.timer = undefined;
+          break;
+      }
+    }
+  },
+  componentWillUnmount: function() {
+    clearInterval(this.timer);
+  },
+  startTimer: function() {
+    this.timer = setInterval(() => {
+      var newCount = this.state.count + 1;
+      this.setState({
+        count: newCount
+      });
+    }, 1000);
+  },
+  handleStatusChange: function(newStatus) {
+    this.setState({
+      timerStatus: newStatus
+    });
+  },
   render: function () {
+    var {timerStatus, count} = this.state;
+
     return (
       <div>
-        Timer.jsx
+        <h1 className="page-title">Timer App</h1>
+        <Clock totalSeconds={count}/>
+        <Controls countdownStatus={timerStatus} onStatusChange={this.handleStatusChange} />
       </div>
     );
   }
